6bb954fe309772df675b91dd06e597cf624025f4
Florian Lorber <laptopuser@florber.ist.intra>
2012-04-20 13:23:29 +0200

    verilog, again

--- src/synthesis/z3.py
+++ src/synthesis/z3.py
@@ -1,11 +1,16 @@
 from helpers.shell import execute_shell
-
+from interfaces.transition import StateTransition
+from interfaces.transition import OutputTransition
 
 class Z3:
     SAT = 0
     UNSAT = 1
     UNKNOWN = 2
     UNDEFINIED = 3
+    
+    tau = None
+    states = None
+    fo = None
 
 
     def __init__(self, path, flag='-'):
@@ -16,9 +21,52 @@ class Z3:
     def get_state(self):
         return self._current_state
 
-    def get_model(self): #TODO: better to return an instance of SmtModel class?
-        return self._model_str
-    
+    def get_model(self):
+        for i in self.states:
+            i=i.replace("(","")
+            i=i.replace(")","")
+            parts=i.split(" ")
+            for j in range(len(self.fo)):
+                self.fo[j]=self.fo[j].replace(parts[1], parts[0] )
+            for j in range(len(self.tau)):
+                self.tau[j]=self.tau[j].replace(parts[1], parts[0] )
+        state = []
+        input = []
+        newState = []
+        for i in self.tau:
+            i=i.replace("(","")
+            i=i.replace(")","")
+            i=i.replace("tau ","")
+            parts=i.split(" ")
+
+            parts[0]=parts[0].replace("t_","")
+            parts[2]=parts[2].replace("t_","")
+            if "_not" in parts[1]:
+                parts[1]=parts[1].replace("_not","")
+                parts[1]+=" == 0"
+            else:
+                parts[1]+=" == 1"
+            state.append(parts[0])
+            input.append(parts[1])
+            newState.append(parts[2])
+        tau = StateTransition(state,input,newState)
+
+        state = []
+        result = []
+        type = []
+        for i in self.fo:
+            i=i.replace("(","")
+            i=i.replace(")","")
+            parts=i.split(" ")
+            parts[1]=parts[1].replace("t_","")
+            type.append(parts[0])
+            state.append(parts[1])
+            result.append(parts[2])
+
+        lo = OutputTransition(type, state, result)
+
+        return [tau,lo]
+
     def solve(self, smtstr):
         rc, output, err = execute_shell(self._cmd, smtstr)
         if err:
@@ -34,14 +82,15 @@ class Z3:
             self._current_state= Z3.SAT
 
             #parse model
-            parse= False
-            self._model_str = ''
+            self.states=[]
+            self.tau = []
+            self.fo=[]
             for line in output_lines:
-                if line == ')':
-                    parse = False         
-                if parse is True:
-                    self._model_str+=line+"\n"
-                if line == '(model':
-                    parse = True
+                if 'tau' in line:
+                    self.tau.append(line)
+                elif 'fo' in line:
+                    self.fo.append(line)
+                elif '(' in line:
+                    self.states.append(line)
         else:
             assert False, 'unknown Z3 state: ' + output
