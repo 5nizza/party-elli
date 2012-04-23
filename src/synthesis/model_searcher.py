436dfe8d85dba5248a9b4e65ac8e9ffbb4de2db2
Ayrat <ayrat.khalimov@tugraz.at>
2012-04-23 11:55:23 +0200

    renamged

--- src/synthesis/model_searcher.py
+++ src/synthesis/model_searcher.py
@@ -15,25 +15,25 @@ def _get_logger():
 def search(uct, inputs, outputs, bound, z3solver):
     assert bound > 0
 
-    _logger = _get_logger()
+    logger = _get_logger()
 
-    _logger.info("searching the model of size <=%s", bound)
+    logger.info("searching the model of size <=%s", bound)
 
     encoder = Encoder(uct, inputs, outputs)
     for current_bound in range(1, bound+1):
-        _logger.info('trying model size = %i', current_bound)
+        logger.info('trying model size = %i', current_bound)
 
         smt_str = encoder.encode_uct(current_bound)
         z3solver.solve(smt_str)
 
         if z3solver.get_state() == Z3.UNSAT:
-            _logger.info('unsat..')
+            logger.info('unsat..')
         elif z3solver.get_state() == Z3.SAT:
-            _logger.info('sat!')
+            logger.info('sat!')
             model = SmtModel(z3solver.get_model())
-            _logger.debug(str(model))
+            logger.debug(str(model))
             return model
         else:
-            _logger.warning('solver status is unknown')
+            logger.warning('solver status is unknown')
 
     return None
