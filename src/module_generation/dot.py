8410fcfc27947345d1dbb39ef630208cb77e1297
Ayrat <ayrat.khalimov@tugraz.at>
2012-07-03 13:20:58 +0200

    improved dot visualization

--- src/module_generation/dot.py
+++ src/module_generation/dot.py
@@ -1,7 +1,19 @@
 def to_dot(smt_module):
     dot_lines = ["digraph module { "]
 
-    state_trans = smt_module.get_model()[0]
+    state_trans, output_transition = smt_module.get_model()
+
+    output = {}
+    for state, out_var, out_val in zip(output_transition.state, output_transition.type, output_transition.result):
+        output[state] = output.get(state, {})
+        output[state][out_var] = out_val
+
+    for state, values_dict in output.items():
+        var_value_pairs = sorted(map(lambda it: (it[0], it[1]), values_dict.items()))
+        values = ' '.join(map(lambda var_val: ['-', ''][var_val[1]=='true'] + var_val[0].replace('fo_', ''),
+                              var_value_pairs))
+        dot_lines.append('"{0}" [label="{1}"{2}]'.format(state, values, ['', ', color="green"'][state=='0']))
+
     for i in range(len(state_trans.state)):
         dot_lines.append('"{0}" -> "{1}" [label="{2}"]'.format(state_trans.state[i],
             state_trans.new_state[i],
