9628c1a4958e5322275fcef8ef5a6ca1c2d31e0a
Ayrat Khalimov <ayrat.khalimov@gmail.com>
2013-06-07 19:12:10 +0200

    reformat

--- src/interfaces/automata.py
+++ src/interfaces/automata.py
@@ -8,7 +8,6 @@ class Automaton:
         self._nodes = set(nodes)
         self._name = name
 
-
     @property
     def name(self):
         return self._name
@@ -23,19 +22,17 @@ class Automaton:
         return self._init_sets_list
 
     @property
-    def rejecting_nodes(self): #TODO: attention: will deprecated for rejecting edges automaton
+    def rejecting_nodes(self):  # TODO: attention: will deprecated for rejecting edges automaton
         return self._rejecting_nodes
-
 
     def __str__(self):
         return self._name + \
                "\nnodes:\n" + \
-               "\n".join([str(x) for x in self._nodes]) +\
-               "\n initial nodes:\n" +\
-               "\n".join([str(x) for x in self._init_sets_list]) +\
+               "\n".join([str(x) for x in self._nodes]) + \
+               "\n initial nodes:\n" + \
+               "\n".join([str(x) for x in self._init_sets_list]) + \
                "\n rejecting nodes:\n" + \
                "\n".join([str(x) for x in self._rejecting_nodes])
-
 
     __repr__ = __str__
 
@@ -62,7 +59,6 @@ class Node:
         """ Return dict { label->[ Set_of_flagged_nodes, Set_of_flagged_nodes..] } """
         return self._transitions
 
-
     def add_transition(self, label, flagged_nodes):
         """ Add transition:
             flagged_nodes - set of pairs (node, is_rejecting)
@@ -73,27 +69,24 @@ class Node:
 
         flagged_nodes_set = set(flagged_nodes)
         assert flagged_nodes_set not in label_transitions, \
-        str(label_transitions) + ', ' + str(flagged_nodes_set) + ', ' + str(label) + ', ' + str(self._name)
+            str(label_transitions) + ', ' + str(flagged_nodes_set) + ', ' + str(label) + ', ' + str(self._name)
 
         label_transitions.append(flagged_nodes_set)
-
 
     def __str__(self):
         labels_strings = []
         for l, dst_list in self.transitions.items():
             dst_strings = []
             for flagged_dst_set in dst_list:
-                dst_strings.append('({0})'.format(str(', '.join(['{0}{1}'.format(n.name, ['',':rej'][is_rejecting])
+                dst_strings.append('({0})'.format(str(', '.join(['{0}{1}'.format(n.name, ['', ':rej'][is_rejecting])
                                                                  for n, is_rejecting in flagged_dst_set]))))
 
             labels_strings.append('[{0}: {1}]'.format(str(l), ', '.join(dst_strings)))
 
         return "'{0}', transitions: {1}".format(self.name, ' '.join(labels_strings))
 
-
     def __lt__(self, other):
         return id(self) < id(other)
-
 
     def __repr__(self):
         return "'{0}'".format(self.name)
