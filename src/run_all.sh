cfb3cf44cd25d954803c009b6fc6e0b22d499edb
ayrat <ayrat.khalimov@gmail.com>
2015-09-18 20:05:53 +0500

    minor

--- src/run_all.sh
+++
@@ -1,3 +0,0 @@
-./run_tests.sh all
-./run_tests.sh all_symbolic
-./run_tests.sh all_symbolic_forall
--- src/run_bfsj.sh
+++
@@ -1,3 +0,0 @@
-./run_tests.sh bfsj
-./run_tests.sh bfsj_symbolic
-./run_tests.sh bfsj_symbolic_forall
