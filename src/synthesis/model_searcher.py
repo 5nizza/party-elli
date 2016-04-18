f9ca6461231cffbc5ab5c8e24e3026ab41a8a5e7
ayrat <ayrat.khalimov@gmail.com>
2016-04-18 14:48:02 +0500

    logger -> logging

--- src/synthesis/model_searcher.py
+++ src/synthesis/model_searcher.py
@@ -6,15 +6,13 @@ from interfaces.lts import LTS
 
 @log_entrance()
 def search(sizes, encoder) -> LTS:
-    logger = logging.getLogger()
-
     max_model_states = list(range(sizes[-1]))
     encoder.encode_headers(max_model_states)
     encoder.encode_initialization()
 
     last_size = 0
     for size in sizes:
-        logger.info('searching a model of size {0}..'.format(size))
+        logging.info('searching a model of size {0}..'.format(size))
 
         cur_all_states = range(size)
         new_states = cur_all_states[last_size:]
