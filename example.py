59ea08b4cb0460a48a830cc917c175a8ae78b00f
ayrat <ayrat.khalimov@gmail.com>
2016-11-11 15:49:44 +0100

    aht

--- example.py
+++ example.py
@@ -4,15 +4,12 @@ from interfaces.spec import Spec
 rs1, r1 = sig_prop('r1')
 gs1, g1 = sig_prop('g1')
 
-rs2, r2 = sig_prop('r2')
-gs2, g2 = sig_prop('g2')
+# rs2, r2 = sig_prop('r2')
+# gs2, g2 = sig_prop('g2')
 
-inputs = [rs1,rs2]
-outputs = [gs1,gs2]
+inputs = [rs1]
+outputs = [gs1]
 
-formula = ~g1 & ~g2 & \
-          AG((r1 >> F(g1)) & (r2 >> F(g2))) & \
-          AG(~(g1&g2)) & \
-          AG(EF(r1 & ~g1 & X(r1 & ~g1 & X(r1 & ~g1)))) & \
-          AG(EF(r2 & ~g2 & X(r2 & ~g2 & X(r2 & ~g2))))
+formula = AGEF(~g1 & ~r1)
+
 spec = Spec(inputs, outputs, formula)
---
+++ experiments-for-tacas/aht.md
@@ -0,0 +1,3 @@
+# AHT automaton for AGEF(~g1~r1)
+
+![](AGEF(ng1nr1.png)
