975092e81686bcd546ac7fc70c141e9eb7bf4b00
ayrat <ayrat.khalimov@gmail.com>
2018-04-04 20:55:53 +0300

    minor

--- module_generation/dot.py
+++ module_generation/dot.py
@@ -115,6 +115,7 @@ def _lts_to_dot(lts:LTS, state_variable, moore_signals):
     return '\n'.join(dot_lines)
 
 
+# TODO: rm state_variable --- use lts.state_name instead
 # TODO: automatically identify type (Moore or Mealy)
 def lts_to_dot(lts:LTS, state_variable, is_mealy) -> str:
     if is_mealy:
